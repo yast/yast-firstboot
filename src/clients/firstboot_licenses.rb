050d3c3c3c334e1c6b28b94dbaf2255c89de9e01
David Díaz González <dgonzalez@suse.de>
2019-05-10 14:13:59 +0100

    Refactor the "firstboot_licenses" client
    This will be the client used when the firstboot.xml's workflow contains
    whichever of below modules
    
      * firstboot_license
      * firstboot_licence_novell
      * firstboot_licenses (itself)
    
    Beyond the code reorganization, to work as is expected it does not take
    care of the default licenses directory anymore. This is already being
    done in the Yast::InstLicenseClient.

--- src/clients/firstboot_licenses.rb
+++ src/clients/firstboot_licenses.rb
@@ -1,79 +1,80 @@
 # encoding: utf-8
 
-# ------------------------------------------------------------------------------
-# Copyright (c) 2012 Novell, Inc. All Rights Reserved.
+# Copyright (c) [2019] SUSE LLC
 #
+# All Rights Reserved.
 #
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of version 2 of the GNU General Public License as published by the
-# Free Software Foundation.
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
 #
-# You should have received a copy of the GNU General Public License along with
-# this program; if not, contact Novell, Inc.
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
 #
-# To contact Novell about this file by physical or electronic mail, you may find
-# current contact information at www.novell.com.
-# ------------------------------------------------------------------------------
-
-# Purpose	: Display 2 license texts (probably from vendor and Novell) during firstboot configuration
-#
-# $Id$
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
 module Yast
+  # Client that allows to display license texts during the firstboot
+  # configuration.
+  #
+  # NOTE: for backward compatibility, it can display the Novell licenses if a
+  # valid path is given through FIRSTBOOT_NOVELL_LICENCE_DIR
   class FirstbootLicensesClient < Client
     def main
-      Yast.import "UI"
       textdomain "firstboot"
 
+      Yast.import "UI"
       Yast.import "Misc"
       Yast.import "GetInstArgs"
-      Yast.import "ProductFeatures"
 
+      result = WFM.CallFunction("inst_license", inst_license_args)
 
-      @result = nil
+      UI.CloseDialog if result == :halt
 
-      @args = GetInstArgs.argmap
+      result
+    end
 
-      # default directory with Novell license texts
-      @default_dir = ProductFeatures.GetStringFeature(
-        "globals",
-        "base_product_license_directory"
-      )
+  private
 
-      @dir1 = Misc.SysconfigRead(
-        path(".sysconfig.firstboot.FIRSTBOOT_LICENSE_DIR"),
-        "/etc/YaST2"
-      )
-      @dir2 = Misc.SysconfigRead(
-        path(".sysconfig.firstboot.FIRSTBOOT_NOVELL_LICENSE_DIR"),
-        @default_dir
-      )
+    # Build, log, and returns args to be used for calling InstLicense client
+    #
+    # @return [Array]
+    def inst_license_args
+      args = GetInstArgs.argmap
+      args["action"] = refusal_action
+      args["directories"] = licenses_directories
 
-      if @dir2 != @dir1
-        Ops.set(@args, "directories", [@dir1, @dir2])
-      else
-        Ops.set(@args, "directories", [@dir1])
-      end
+      Builtins.y2milestone("inst_license options: %1", args)
 
-      Ops.set(
-        @args,
-        "action",
-        Misc.SysconfigRead(
-          path(".sysconfig.firstboot.LICENSE_REFUSAL_ACTION"),
-          "abort"
-        )
-      )
+      [args]
+    end
 
-      Builtins.y2milestone("inst_license options: %1", @args)
+    # Action to perform if the user's refusal to accept the license agreement
+    #
+    # @return [String]
+    def refusal_action
+      Misc.SysconfigRead(path(".sysconfig.firstboot.LICENSE_REFUSAL_ACTION"), "abort")
+    end
 
-      @result = WFM.CallFunction("inst_license", [@args])
+    # Directories in which look for the license agreement texts
+    #
+    # NOTE: if that result in an empty list, {Yast::InstLicenseClient} will do
+    # an extra attemp to look for the license agreement in the path given as
+    # "base_product_license_directory" global param through the control file.
+    #
+    # @return [Array<String>] license agreement paths
+    def licenses_directories
+      directories = [
+        Misc.SysconfigRead(path(".sysconfig.firstboot.FIRSTBOOT_LICENSE_DIR"), ""),
+        Misc.SysconfigRead(path(".sysconfig.firstboot.FIRSTBOOT_NOVELL_LICENSE_DIR"), "")
+      ]
 
-      UI.CloseDialog if @result == :halt
-
-      deep_copy(@result)
+      directories.uniq.reject(&:empty?)
     end
   end
 end
