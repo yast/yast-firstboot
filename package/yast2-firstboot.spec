a6b82e246489715df9b9ce41c263e46a33b85931
Stefan Schubert <schubi@suse.de>
2019-10-11 12:10:05 +0200

    adapted to refactored keyboard module

--- package/yast2-firstboot.spec
+++ package/yast2-firstboot.spec
@@ -16,7 +16,7 @@
 #
 
 Name:           yast2-firstboot
-Version:        4.2.3
+Version:        4.2.4
 Release:        0
 Summary:        YaST2 - Initial System Configuration
 Group:          System/YaST
--- src/clients/firstboot.rb
+++ src/clients/firstboot.rb
@@ -59,7 +59,7 @@ module Yast
       # generated yet (the X server started by YaST-Firstboot doesn't seem to
       # be enough to trigger the systemd mechanism that generates it), let's
       # enforce the keyboard map if we are running in graphic mode (bsc#950335)
-      Keyboard.SetX11(Keyboard.current_kbd)
+      Keyboard.Set(Keyboard.current_kbd)
 
       # initialize package callbacks, since some of the modules run in the
       # firstboot workflow expect them to be initialized (bug #335979)
